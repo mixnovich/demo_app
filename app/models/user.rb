70d7a3cf717253000a2056e6b4f9e15ab8cc5367
Mixnovich Barram <mixnovich@yahoo.com>
2012-08-23 00:19:13 +0200

    finished demo app

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
   attr_accessible :email, :name
+     validates :name, :length => {:maximum => 20}
+     validates :email, :length => {:maximum => 20}
+	has_many :microposts
+
 end
